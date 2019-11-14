c4a9b5fd3e9664864a83b1bbd64859d193384c4a
Murray Cadzow <murray.cadzow@otago.ac.nz>
2019-11-14 14:38:07 +1300

    Initial commit

---
+++ R/example.R
@@ -0,0 +1,39 @@
+#' Always return three
+#'
+#' @return 3
+#' @export
+#'
+#' @examples return_three()
+return_three <- function() {
+  3
+}
+
+
+#' Multiply by three
+#' Multiplies a given number by three
+#'
+#' @param n
+#'
+#' @return number
+#' @export
+#'
+#' @examples multiply_three(4)
+multiply_three <- function(n) {
+  n * 3
+}
+
+
+#' Add numbers
+#'
+#' @param x a number to be added.
+#' @param y another number
+#'
+#' @return
+#' @export
+#'
+#' @examples add_numbers(3, 4)
+add_numbers <- function(x, y) {
+  x + y
+}
+
+
